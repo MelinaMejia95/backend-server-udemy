cd0519b958a4a1a6405e327213c4fe39859c7e0c
Mejía Bedoya <melinamejia95@gmail.com>
2018-08-31 10:18:33 -0500

    Delete Usuarios service done

--- routes/usuario.js
+++ routes/usuario.js
@@ -102,5 +102,35 @@ app.put('/:id', (req, res) => {
 
 });
 
+//Eliminar usuario
+app.delete('/:id', (req, res) => {
+
+    var id = req.params.id;
+
+    Usuario.findByIdAndRemove( id, (err, usuarioBorrado) => {
+        if (err) {
+            return  res.status(500).json({
+                ok: false,
+                mensaje: 'Error al eliminar usuario',
+                errors: err
+            });
+        }
+
+        if (!usuarioBorrado) {
+            return  res.status(400).json({
+                ok: false,
+                mensaje: 'No existe un usuario con ese id',
+                errors: { message: 'No existe un usuario con ese id' }
+            });
+        }
+
+        res.status(200).json({
+            ok: true,
+            usuario: usuarioBorrado
+        });
+    });
+
+});
+
 
 module.exports = app;
