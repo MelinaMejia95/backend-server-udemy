13fbeb405890ee21f54a4923dba8f746fe72d46b
Mejía Bedoya <melinamejia95@gmail.com>
2018-08-31 10:12:11 -0500

    Update Usuarios service done

--- routes/usuario.js
+++ routes/usuario.js
@@ -54,4 +54,53 @@ app.post('/', (req, res) => {
 
 });
 
+//Actualizar usuario
+app.put('/:id', (req, res) => {
+
+    var id = req.params.id;
+    var body = req.body;
+    
+    Usuario.findById( id, (err, usuario) => {
+        if (err) {
+            return  res.status(500).json({
+                ok: false,
+                mensaje: 'Error al buscar usuario',
+                errors: err
+            });
+        }
+
+        if( !usuario ) {
+            return  res.status(400).json({
+                ok: false,
+                mensaje: 'El usuario con el id ' + id + ' no existe',
+                errors: { message: 'No existe un usuario con ese ID' }
+            });
+        }
+
+        usuario.nombre = body.nombre;
+        usuario.email = body.email;
+        usuario.role = body.role;
+
+        usuario.save( (err, usuarioGuardado) => {
+            if (err) {
+                return  res.status(400).json({
+                    ok: false,
+                    mensaje: 'Error al actualizar usuario',
+                    errors: err
+                });
+            }
+
+            usuarioGuardado.password = ':)';
+
+            res.status(200).json({
+                ok: true,
+                usuario: usuarioGuardado
+            });
+        });
+
+    });
+
+});
+
+
 module.exports = app;
