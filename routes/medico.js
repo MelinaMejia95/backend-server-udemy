9df8b486d8fe6b6ac2d5d045b86da5b4e94175a4
Mejía Bedoya <melinamejia95@gmail.com>
2018-09-10 11:22:00 -0500

    Get Medico by ID

--- routes/medico.js
+++ routes/medico.js
@@ -38,6 +38,39 @@ app.get('/', (req, res) => {
 
 });
 
+//Obtener médico
+app.get('/:id', (req, res) => {
+
+    var id = req.params.id;
+    Medico.findById( id )
+        .populate('usuario', 'nombre email img')
+        .populate('hospital')
+        .exec( (err, medico) => {
+
+            if (err) {
+                return res.status(500).json({
+                    ok: false,
+                    mensaje: 'Error al buscar médico',
+                    errors: err
+                });
+            }
+    
+            if (!medico) {
+                return res.status(400).json({
+                    ok: false,
+                    mensaje: 'El médico con id ' + id + ' no existe',
+                    errors: { message: 'No existe un médico con ese ID' }
+                });
+            }
+
+            res.status(200).json({
+                ok: true,
+                medico: medico
+            });
+
+        })
+});
+
 //Actualizar médico
 app.put('/:id', mdAutenticacion.verificaToken, (req, res) => {
     
